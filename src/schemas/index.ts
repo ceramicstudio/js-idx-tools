64f256ad30b20c183025075e734f52a445e3b3a7
Paul Le Cam <paul@3box.io>
2020-09-02 10:19:31 +0100

    Add Definition and DocIdList schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,8 @@
 import BasicProfile from './BasicProfile.json'
+import Definition from './Definition.json'
 import DocIdDocIdMap from './DocIdDocIdMap.json'
+import DocIdList from './DocIdMap.json'
 import DocIdMap from './DocIdMap.json'
 import StringMap from './StringMap.json'
 
-export { BasicProfile, DocIdDocIdMap, DocIdMap, StringMap }
+export { BasicProfile, Definition, DocIdDocIdMap, DocIdList, DocIdMap, StringMap }
