00fd458a367f77d6524495e8bbc62961c2f067ba
Sergey Ukustov <sergey@ukstv.me>
2020-09-18 13:51:01 +0400

    Add CryptoAccountLinks schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,5 +4,14 @@ import DocIdDocIdMap from './DocIdDocIdMap.json'
 import DocIdMap from './DocIdMap.json'
 import IdentityIndex from './IdentityIndex.json'
 import StringMap from './StringMap.json'
+import CryptoAccountLinks from './CryptoAccountLinks.json'
 
-export { BasicProfile, Definition, DocIdDocIdMap, DocIdMap, IdentityIndex, StringMap }
+export {
+  BasicProfile,
+  Definition,
+  DocIdDocIdMap,
+  DocIdMap,
+  IdentityIndex,
+  StringMap,
+  CryptoAccountLinks,
+}
