2c132c56c9dfdb8042895f06da384ec4dd3dffc9
Paul Le Cam <paul@3box.io>
2020-09-28 14:03:43 +0100

    Remove wallet from deploy script

--- scripts/author.js
+++ scripts/author.js
@@ -1,14 +1,26 @@
 const { DID } = require('dids')
 const { resolve } = require('path')
+const Wallet = require('identity-wallet').default
 
 const { createIDXSignedDefinitions, publishIDXSignedSchemas, signIDXSchemas } = require('../dist')
-const { ceramic, createWallet, writeSigned } = require('./common')
+const { ceramic, writeSigned } = require('./common')
 
 const DEFINITIONS_PATH = resolve(__dirname, '../src/signed/definitions.json')
 const SCHEMAS_PATH = resolve(__dirname, '../src/signed/schemas.json')
 
+const seed = process.env.SEED
+if (!seed) {
+  throw new Error('Missing SEED environment variable')
+}
+
 async function run() {
-  const wallet = await createWallet()
+  const wallet = await await Wallet.create({
+    ceramic,
+    seed,
+    getPermission() {
+      return Promise.resolve([])
+    },
+  })
   const did = new DID({ provider: wallet.getDidProvider() })
 
   await did.authenticate()
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,7 @@
 const { publishIDXConfig } = require('..')
-const { ceramic, createWallet, logJSON } = require('./common')
+const { ceramic, logJSON } = require('./common')
 
 async function run() {
-  const wallet = await createWallet()
-  await ceramic.setDIDProvider(wallet.getDidProvider())
-  console.log('Connected to Ceramic')
-
   const config = await publishIDXConfig(ceramic)
   console.log('IDX config published:')
   logJSON(config)
