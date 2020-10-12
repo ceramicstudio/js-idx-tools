969534d3d6e0b3bca162845c51e10112fd4f05ac
Paul Le Cam <paul@3box.io>
2020-10-12 15:19:32 +0100

    Add constants for 3ID keychain

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ import type { IDXPublishedDefinitions, IDXPublishedSchemas } from './types'
 export const publishedDefinitions: IDXPublishedDefinitions = {
   basicProfile: 'ceramic://bagcqcerakszw2vsovxznyp5gfnpdj4cqm2xiv76yd24wkjewhhykovorwo6a',
   cryptoAccountLinks: 'ceramic://bagcqcerab3lf5regasdioanst6buv7qlbxtj3kfnq2uzajaaynxsxjdeoedq',
+  threeIdKeychain: 'ceramic://bagcqceralomo65ag34ymh37ciaclr2qrum37b74jwyzawf23r2iypuufpjya',
 }
 
 export const publishedSchemas: IDXPublishedSchemas = {
@@ -10,4 +11,5 @@ export const publishedSchemas: IDXPublishedSchemas = {
   CryptoAccountLinks: 'ceramic://bagcqcerapvke7vlokqdemay2rfd36rdg6azhqe37xum76nwjmam2bnozssqa',
   Definition: 'ceramic://bagcqcerayzl65zpukldtsp2bgy4rhsvdagkba27osgzepnw3xg5bhzg4dcwq',
   IdentityIndex: 'ceramic://bagcqceragcfkpjmv722fdse3sfhvx2jcezlb2avvg63fmxbkjoczhsq3rlfa',
+  ThreeIdKeychain: 'ceramic://bagcqcerawqxf7b6s2obd2gpdzzu3yl47metomi4oxm7kszrojbuxfuv5grwa',
 }
